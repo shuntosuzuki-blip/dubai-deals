e4651ca1b33c1bd463063b360b0307ac56dccab8
shuntosuzuki-blip <shunto.suzuki@gmail.com>
2026-03-27 17:29:46 +0400

    add next.config.js

--- next.config.js
+++ next.config.js
@@ -1,17 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  async headers() {
-    return [
-      {
-        source: '/api/:path*',
-        headers: [
-          { key: 'Access-Control-Allow-Origin', value: '*' },
-          { key: 'Access-Control-Allow-Methods', value: 'GET,OPTIONS' },
-        ],
-      },
-    ]
+  i18n: {
+    locales: ['en', 'ja'],
+    defaultLocale: 'en',
+  },
+  images: {
+    remotePatterns: [
+      { protocol: 'https', hostname: '*.bayut.com' },
+      { protocol: 'https', hostname: 'bayut-production.s3.eu-central-1.amazonaws.com' },
+    ],
   },
 }
-
 module.exports = nextConfig
